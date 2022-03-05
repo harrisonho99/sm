e889f66713964234733186d280731a1ffd53ee9e
hotsnow199 <socrom001@gmail.com>
2022-03-05 12:50:20 +0700

    remove call pkg function

--- src/main.go
+++ src/main.go
@@ -3,14 +3,5 @@ A simpler test module
 */
 package main
 
-import (
-	"fmt"
-	"sm/pkg/dslice"
-)
-
 func main() {
-	sl := dslice.NewSlice()
-	dslice.SliceAppend(sl, 30)
-	dslice.SliceAppend(sl, 10)
-	fmt.Println(sl)
 }
